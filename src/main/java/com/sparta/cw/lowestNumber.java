412a755a507e19878d75d0194c8f42406ca453be
Craig Woodcock <cwoodcock@spartaglobal.com>
2023-11-30 00:10:15 +0000

    lowestNumber prints array before and after Arrays.sort

--- src/main/java/com/sparta/cw/lowestNumber.java
+++ src/main/java/com/sparta/cw/lowestNumber.java
@@ -1,6 +1,7 @@
 package com.sparta.cw;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class lowestNumber {
     public static void main(String[] args) {
@@ -10,7 +11,9 @@ public class lowestNumber {
     private static int getLowestNumber() {
         int[] arr = {12,23,456,3,15,189,56,48,5,4};
         try{
+            System.out.println(Arrays.toString(arr));
             Arrays.sort(arr);
+            System.out.println(Arrays.toString(arr));
 
         }catch(IndexOutOfBoundsException e){
             System.out.println(e.getMessage());
